893a14fa9d7bd43e3dbbe7b142de1cc09b61699b
PNJaenichen <PNJaenichen@gmail.com>
2021-01-22 23:08:01 -0500

    In progress of library project, day 2

--- library/library.js
+++ library/library.js
@@ -1,12 +1,3 @@
-
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach((button => {
-    button.addEventListener('click', () => {
-        alert(button.id);
-    });
-}));
-
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -19,6 +10,41 @@ Book.prototype.info = function() {
     console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`);
 }
 
-function addBookToLibrary() {
-    
+// TODO Function that adds user input to array
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
 }
+
+addBookToLibrary('Rhythm of War', 'Brandon Sanderson', 1232, false);
+addBookToLibrary("A Wise Man's Fear",'Patrick Rothfuss',994,true);
+addBookToLibrary("Chesty", "Jon T. Hoffman", 676, false);
+for (let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i]);
+}
+
+let library = document.getElementById('bookList');
+for (let i = 0; i < myLibrary.length; i++) {
+    let element = document.createElement('div');
+    element.classList.add('card');
+    element.innerHTML = `<strong>${myLibrary[i].title}</strong>`;
+    for (let [key, value] of Object.entries(myLibrary[i])) {
+        if (key === 'title') {
+            continue;
+        } else {
+            let para = document.createElement('p');
+            para.innerHTML = `${key}: ${value}`;
+            element.append(para);
+        }
+    }
+    let readButton = document.createElement('button');
+    readButton.innerHTML = 'READ';
+    let removeButton = document.createElement('button');
+    removeButton.innerHTML = 'REMOVE';
+    element.append(readButton);
+    element.append(removeButton);
+    library.append(element);
+}
+
+// TODO Add a button to remove a book
+
+// TODO Add a button that changes the read value of a book
